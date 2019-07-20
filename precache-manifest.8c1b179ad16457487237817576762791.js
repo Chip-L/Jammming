eb07b7008e477341d04f45523e113bc70583ead7
Chip Long <chip_long@yahoo.com>
2019-07-20 00:41:08 -0600

    Updates

---
+++ precache-manifest.8c1b179ad16457487237817576762791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3262429788f773733e1af6b774b8838",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "e0e794b2ba9b79120054",
+    "url": "/Jammming/static/css/main.5d1a9d88.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Jammming/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e0e794b2ba9b79120054",
+    "url": "/Jammming/static/js/main.7d336c4c.chunk.js"
+  },
+  {
+    "revision": "6bf7feb5370d6eaee948",
+    "url": "/Jammming/static/js/runtime~main.23f8de95.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jammming/precache-manifest.37442133718feb5db2fa478cdcc6cf29.js"
+  "/Jammming/precache-manifest.8c1b179ad16457487237817576762791.js"
 );
 
 self.addEventListener('message', (event) => {
