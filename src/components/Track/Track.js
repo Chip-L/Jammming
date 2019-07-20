7f8f3801b4763fa842d1349042c0dd3ad70c4bde
Chip Long <chip_long@yahoo.com>
2019-07-20 01:20:01 -0600

    setup Track

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -7,7 +7,23 @@ export class Track extends React.Component {
     super(props);
   }
 
+  renderAction() {
+    if(this.props.isRemoval) {
+      return '-';
+    } else {
+      return '+';
+    };
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3> track name will go here </h3>
+          <p> track artist will go here | track album will go here </p>
+        </div>
+        <button className="Track-action">{renderAction}</button>
+      </div>
+    );
   }
 };
