2896f689bf12703fd67610cdf483e7fb4a0ee402
Chip Long <chip_long@yahoo.com>
2019-07-19 23:45:06 -0600

    Updates

---
+++ precache-manifest.37442133718feb5db2fa478cdcc6cf29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9456f04c886fcc7c167f7fc0ef3d8857",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "b6c0030fc61efa0ab203",
+    "url": "/Jammming/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Jammming/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b6c0030fc61efa0ab203",
+    "url": "/Jammming/static/js/main.5ed7f43b.chunk.js"
+  },
+  {
+    "revision": "6bf7feb5370d6eaee948",
+    "url": "/Jammming/static/js/runtime~main.23f8de95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jammming/static/media/logo.5d5d9eef.svg"
+  }
+]);
