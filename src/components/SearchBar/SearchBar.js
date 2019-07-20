bc9c37fee3bc0a8ec78e2b18b2ca323207dd8212
Chip Long <chip_long@yahoo.com>
2019-07-20 00:47:35 -0600

    setup SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,8 +3,9 @@ import React from 'react';
 export class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        Search Bar
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <button className="SearchButton">SEARCH</button>
       </div>
     );
   }
