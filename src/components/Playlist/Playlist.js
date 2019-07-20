3a88591c93dd08801511743feefab401dea8547f
Chip Long <chip_long@yahoo.com>
2019-07-20 00:39:49 -0600

    Playlist, SearchBar, and SearchResults placeholders constructed-no css

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Playlist extends React.Component {
+/*
+  constructor() {
+
+  }
+*/
+
+  render() {
+    return (
+      <div>
+        Play List
+      </div>
+    );
+  }
+};
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Search Results
+      </div>
+    );
+  }
+};
